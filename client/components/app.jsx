29ada2253b16fab5dbcddef37ebb138becdfaec6
Kevin Lenell <krlenell@gmail.com>
2020-07-24 13:00:28 -0700

    styled and displayed the product details

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import Header from './header.jsx';
 import ProductList from './product-list.jsx';
+import ProductDetails from './product-details.jsx';
 
 export default class App extends React.Component {
 
   render() {
     return (
-      <>
-        <Header/>
-        <ProductList/>
-      </>
+      // <>
+      //   <Header/>
+      //   <ProductList/>
+      // </>
+      <ProductDetails/>
     );
   }
 }
--- client/components/product-details.jsx
+++ client/components/product-details.jsx
@@ -9,16 +9,57 @@ export default class ProductDetails extends React.Component {
   }
 
   componentDidMount() {
+    this.getProduct();
+  }
+
+  getProduct() {
     fetch('/api/products/1', {
       headers: {
         'Content-Type': 'application/json'
       }
+    })
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          product: data
+        }, () => console.log(this.state));
+      });
+  }
+
+  parsePrice(cost) {
+    let returnPrice = cost.toString(10);
+    const length = returnPrice.length;
+    returnPrice = '$' + returnPrice.substring(0, length - 2) + '.' + returnPrice.substring(length - 2, length);
+    return returnPrice;
+  }
+
+  parseParagraphs(string) {
+    const words = string.split('\\n');
+    console.log(words);
+    const paragraphs = words.map((paragraph, index) => {
+      return <p key={index}>{paragraph}</p>;
     });
+    console.log(paragraphs);
+    return paragraphs;
   }
 
   render() {
+    if (!this.state.product) {
+      return null;
+    }
     return (
-      null
+      <div className="card m-5">
+        <p className="back-button text-muted m-2">&lt; Back to catalog</p>
+        <div className="product-head d-flex">
+          <img src={this.state.product.image} height="300px" alt="shake-weight.jpg"/>
+          <div>
+            <h1 className="card-title">{this.state.product.name}</h1>
+            <h3 className="card-subtitle text-muted">{this.parsePrice(this.state.product.price)}</h3>
+            <p className="card-text">{this.state.product.shortDescription}</p>
+          </div>
+        </div>
+        <div className="card-text m-2">{this.parseParagraphs(this.state.product.longDescription)}</div>
+      </div>
     );
   }
 }
