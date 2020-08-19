92b727598f866e4d301944ec65dc54c14afc4c30
Kevin Lenell <krlenell@gmail.com>
2020-08-18 17:17:05 -0700

    created checkoutForm.jsx

---
+++ client/components/checkout-form.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class CheckoutForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      creditCard: '',
+      shippingAddress: ''
+    };
+  }
+}
